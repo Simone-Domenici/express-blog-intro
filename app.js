6a3e0e23fcab062634d477926eef06050a18f499
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-18 15:52:10 +0100

    ADD route /bacheca

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 });
 
+app.get('/bacheca', (req, res) => {
+    res.json({
+        posts: posts,
+        conteggio: posts.length
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
